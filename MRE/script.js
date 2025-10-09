efd5b237404dacd4559f3f8847a183ef7f45ee61
Le-herisson <168847121+Le-herisson@users.noreply.github.com>
2025-10-09 22:00:59 +0200

    Updated 10 files

--- MRE/script.js
+++ MRE/script.js
@@ -1,30 +1,23 @@
-// Matrix characters
 const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ~@#|&é'(-è_çà)=}]^\`[{¤$£^¨*µù%!§:/;.,?";
 const charactersArray = characters.split("");
 
-// Matrix configuration
 const fontSize = 16;
 const speed = 3;
 
-// Create the canvas and context
 const canvas = document.getElementById("matrix");
 const context = canvas.getContext("2d");
 
-// Set the canvas dimensions
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Calculate the number of columns and rows
 const columns = Math.floor(canvas.width / fontSize);
 const rows = Math.floor(canvas.height / fontSize);
 
-// Initialize the columns
 const matrix = [];
 for (let i = 0; i < columns; i++) {
   matrix[i] = 1;
 }
 
-// Matrix animation
 function drawMatrix() {
   context.fillStyle = "rgba(0, 10, 10, 0.03)";
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -44,11 +37,9 @@ function drawMatrix() {
   }
 }
 
-// Render loop
 function animateMatrix() {
   drawMatrix();
   requestAnimationFrame(animateMatrix);
 }
 
-// Start the animation
 animateMatrix();
---
+++ src/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("contextmenu", (e) => { e.preventDefault(); });
+
+function get_video_id(input, type) {
+    try {
+        switch (type) {
+            case "short":
+                return input.match(/https\:\/\/youtu\.be\/([a-zA-Z0-9-_]{11})/)[1];
+
+            case "long":
+                return input.match(/(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/shorts\/))([a-zA-Z0-9-_]{11})/)[1];
+
+            case "id":
+                return input.match(/([a-zA-Z0-9-_]{11})/g)[0];
+
+            default:
+                throw new Error("Error Invalid youtube link type!");
+    }      
+    } catch (e) {
+        throw new Error("Error Invalid youtube link!\nErr : " + e);
+    }
+     
+}
