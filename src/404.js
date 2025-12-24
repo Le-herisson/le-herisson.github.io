7b298ba84a2ad5cc3bb60ee2ec81d87e2bfbc86c
Le-herisson <168847121+Le-herisson@users.noreply.github.com>
2025-12-24 11:28:12 +0100

    fixed the 404.js script...

--- src/404.js
+++ src/404.js
@@ -2,7 +2,6 @@ document.addEventListener("contextmenu", (e) => { e.preventDefault(); });
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('go-button').addEventListener("click",()=>{
         let url=`https://le-herisson.github.io/${document.getElementById('url-field').value}`;
-        if(confirm(`Go to: ${url} ?`))
-        location.href=url;
+        if(confirm(`Go to: ${url} ?`)){location.href=url;}
     });
 });
