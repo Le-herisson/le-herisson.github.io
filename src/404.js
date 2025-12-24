411bb3c3fae7d3fad5c8fbd1792f370161b17fe7
Le-herisson <168847121+Le-herisson@users.noreply.github.com>
2025-12-24 01:55:30 +0100

    update 1 file

--- src/404.js
+++ src/404.js
@@ -1,6 +1,8 @@
 document.addEventListener("contextmenu", (e) => { e.preventDefault(); });
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('go-button').addEventListener("click",()=>{
-        location.href=`https://le-herisson.github.io/${document.getElementById('url-field').value}`;
+        let url=`https://le-herisson.github.io/${document.getElementById('url-field').value}`;
+        if(confirm(`Go to: ${url} ?`))
+        location.href=url;
     });
 });
