5a20364d8280c466f98afef19fd3a75ced5ad815
Le-herisson <168847121+Le-herisson@users.noreply.github.com>
2026-03-23 18:51:50 +0100

    bug fix in the new ac.html page

--- tests/ac.js
+++ tests/ac.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    setTimeout(()=>{
-        window.close();
-    },1000);
+    document.getElementById("btn_close").addEventListener("click",()=>{
+        setTimeout(()=>{
+            window.close();
+        },1000);
+    });
 });
