aee2081f93056d681dd48f645f8fcf2e2bcabab5
Le-herisson <168847121+Le-herisson@users.noreply.github.com>
2026-06-16 20:02:27 +0200

    Update file.js

--- tests/file.js
+++ tests/file.js
@@ -1,16 +1,18 @@
-const input = document.createElement('input')
-input.type = 'file'
-input.webkitdirectory = true
-// Fallback to multiple files input for iOS Safari
-input.multiple = true
-// See https://stackoverflow.com/questions/47664777/javascript-file-input-onchange-not-working-ios-safari-only
-input.style.position = 'fixed'
-input.style.top = '-100000px'
-input.style.left = '-100000px'
-document.body.appendChild(input)
+function openFolder(){
+    const input = document.createElement('input')
+    input.type = 'file'
+    input.webkitdirectory = true
+    // Fallback to multiple files input for iOS Safari
+    input.multiple = true
+    // See https://stackoverflow.com/questions/47664777/javascript-file-input-onchange-not-working-ios-safari-only
+    input.style.position = 'fixed'
+    input.style.top = '-100000px'
+    input.style.left = '-100000px'
+    document.body.appendChild(input)
 
-input.click()
-input.addEventListener('change', (evt)=>{
-    alert(evt.toString())
-    input.remove()
-})
+    input.click()
+    input.addEventListener('change', (evt)=>{
+        alert(evt.toString())
+        input.remove()
+    })
+}
