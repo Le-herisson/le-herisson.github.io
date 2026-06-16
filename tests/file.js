23dc9cb1f5e667d6d1abaf97f1352745ca920422
Le-herisson <168847121+Le-herisson@users.noreply.github.com>
2026-06-16 19:56:39 +0200

    Create file.js

---
+++ tests/file.js
@@ -0,0 +1,16 @@
+const input = document.createElement('input')
+input.type = 'file'
+input.webkitdirectory = true
+// Fallback to multiple files input for iOS Safari
+input.multiple = true
+// See https://stackoverflow.com/questions/47664777/javascript-file-input-onchange-not-working-ios-safari-only
+input.style.position = 'fixed'
+input.style.top = '-100000px'
+input.style.left = '-100000px'
+document.body.appendChild(input)
+
+input.click()
+input.addEventListener('change', (evt)=>{
+    alert(evt.toString())
+    input.remove()
+})
