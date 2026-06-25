503047286c3ccadcc26af0602add24be635fc845
Le-herisson <168847121+Le-herisson@users.noreply.github.com>
2026-06-25 17:23:07 +0200

    commit #300

---
+++ sl/s.js
@@ -0,0 +1,26 @@
+async function fetchApi() {
+    return await fetch("/Api/conf.json", {
+                        method: "GET"
+                    })
+                    .then(async(r)=>{
+                        return await r.text();
+                    })
+                    .catch((e)=>{
+                        document.getElementById("box").innerText = "The parameter is invalid or missing: " + e;
+                    }); 
+}
+
+document.addEventListener("DOMContentLoaded", async()=>{
+    const params = new URLSearchParams(window.location.search);
+    const o = params.get('o');
+    if( o !== "" && o.split("_")[1].length == 8 ) {
+        const _res = await JSON.parse(fetchApi());
+        if ( _res.availablesSrc[o.split("_")[0]].contains(o.split("_")[1]) ) {
+            setTimeout(()=>{
+                window.location.href = _res.sources[o.split("_")[1]];
+            },250);
+        }
+    }else{
+        document.getElementById("box").innerText="The parameter is invalid or missing";
+    }
+});
